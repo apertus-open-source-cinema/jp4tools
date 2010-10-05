02fe311a73de6552d286dcb37a1f86979d7a9812
oneartplease <oneartplease@46015a06-04f4-4860-9c33-90e0f15d9639>
2010-10-05 17:58:08 +0000

    clean up
    git-svn-id: https://elphel.svn.sourceforge.net/svnroot/elphel/tools@183 46015a06-04f4-4860-9c33-90e0f15d9639

---
+++ Movie2DNGGUI/src/movie2dnggui/Movie2DNGGUIApp.java
@@ -0,0 +1,44 @@
+/*
+ * Movie2DNGUIApp.java
+ */
+
+package movie2dnggui;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class Movie2DNGGUIApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new Movie2DNGGUIView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of Movie2DNGUIApp
+     */
+    public static Movie2DNGGUIApp getApplication() {
+        return Application.getInstance(Movie2DNGGUIApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(Movie2DNGGUIApp.class, args);
+    }
+}
